e830483c9d3127d37bdbb6714b328c326df4b7f6
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-01 03:35:17 +0700

    sif

--- standup.bash
+++ standup.bash
@@ -23,4 +23,11 @@ osmosisd init notional
 wget -O ~/.osmosisd/config/genesis.json https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json
 
 
-# 
+# Install sif
+wget https://github.com/Sifchain/sifnode/releases/download/mainnet-0.9.0/sifnoded-mainnet-0.9.0-linux-amd64.zip
+unzip sifnoded-mainnet-0.9.0-linux-amd64.zip
+mv sifnoded /usr/bin
+sifnoded init notional
+wget https://github.com/Sifchain/networks/raw/master/mainnet/sifchain-1/genesis.json.gz
+gunzip -k genesis.json.gz
+mv genesis.json ~/.sifnoded/config
