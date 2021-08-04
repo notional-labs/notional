6d22e1aa7ac73940ad61d01a30545eed00c95334
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-04 14:06:01 +0700

    Update standup.bash

--- standup.bash
+++ standup.bash
@@ -63,6 +63,20 @@ regen init notional --chain-id regen-1
 curl http://104.131.169.70:26657/genesis | jq .result.genesis > ~/.regen/config/genesis.json
 
 
+# Install Iris
+git clone https://github.com/irisnet/irishub
+cd irishub
+make install
+iris init notional
+curl -o ~/.iris/config/genesis.json https://raw.githubusercontent.com/irisnet/mainnet/master/config/genesis.json
+curl -o ~/.iris/config/config.toml https://raw.githubusercontent.com/irisnet/mainnet/master/config/config.toml
+cd ~/.iris
+aria2c -x5 https://data.bitcat365.com/irisnet/data.tar.lz4
+lz4 -d  data.tar.lz4  | tar xzf -       
+
+
+
+
 
 
 
