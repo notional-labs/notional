e4784a123890a0d38ce8b0ac42c85f943e5ef390
Jacob Gadikian <jacobgadikian@gmail.com>
2021-09-15 16:38:57 +0200

    yep this is the best way I have found.

---
+++ rly/aktandhub.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+	rly tx relay-packets hubakt
+	rly tx relay-packets akthub
+done
---
+++ rly/osmoandhub.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+	rly tx relay-packets osmohub
+	rly tx relay-packets hubosmo
+done
