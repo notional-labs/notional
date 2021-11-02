2a548da8372bad0c7b8e042d0889a511bfddd383
Jacob Gadikian <jacobgadikian@gmail.com>
2021-11-03 02:24:32 +0700

    Update standup.bash

--- infrastructure/standup.bash
+++ infrastructure/standup.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+export GOPATH=~/go
+export PATH=$PATH:~/go/bin
+
+echo 'GOPATH=~/go' >> ~/.bash_profile
+echo 'PATH=$PATH:~/go/bin' >> ~/.bash_profile
+echo 'PATH=$PATH:~/.cargo/bin' >> ~/.bash_profile
+
 # Keys for arch packages
 pacman -Syyu archlinux-keyring
 
 
 # Utilities needed for smooth nodes
-pacman -Syyu aria2 atop autoconf automake base binutils bison bmon btrfs-progs clang cronie cryptsetup docker dstat fakeroot flex gcc git go gptfdisk groff grub haveged htop iftop iptraf-ng jq llvm lvm2 m4 make mdadm neovim net-tools nethogs openssh patch pkgconf python rsync rustup screen sudo texinfo unzip vi vim vnstat wget which xfsprogs hddtemp python-setuptools npm python-bottle python-docker python-matplotlib python-netifaces python-zeroconf python-pystache time nload nmon glances gtop bwm-ng bpytop duf go-ipfs fish pigz zerotier-one sysstat github-cli pm2
+pacman -Syyu aria2 atop autoconf automake base binutils bison bmon btrfs-progs btop clang cronie cryptsetup docker dstat fakeroot flex gcc git go gptfdisk groff grub haveged htop iftop iptraf-ng jq llvm lvm2 m4 make mdadm neovim net-tools nethogs openssh patch pkgconf python rsync rustup screen sudo texinfo unzip vi vim vnstat wget which xfsprogs hddtemp python-setuptools npm python-bottle python-docker python-matplotlib python-netifaces python-zeroconf python-pystache time nload nmon glances gtop bwm-ng bpytop duf go-ipfs fish pigz zerotier-one sysstat github-cli pm2
 
 
 
