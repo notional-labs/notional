d10c9448b3f2864c0a98cb5aca716e9038f2ded0
Long <77162856+mgialong215@users.noreply.github.com>
2022-09-13 05:11:21 +0700

    Update secretnetwork.sh

--- infrastructure/statesync/secretnetwork.sh
+++ infrastructure/statesync/secretnetwork.sh
@@ -22,6 +22,7 @@ LATEST_HEIGHT=$(curl -s https://rpc.cosmos.directory:443/secretnetwork/block | j
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
 TRUST_HASH=$(curl -s "https://rpc.cosmos.directory:443/secretnetwork/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
+# If the default RPC fails pls replace all with http://secret.rpc.consensus.one:443 (Thank you John!)
 
 # TELL USER WHAT WE ARE DOING
 echo "TRUST HEIGHT: $BLOCK_HEIGHT"
@@ -31,7 +32,7 @@ echo "TRUST HASH: $TRUST_HASH"
 # export tate sync vars
 export SECRETD_STATESYNC_ENABLE=true
 export SECRETD_P2P_MAX_NUM_OUTBOUND_PEERS=200
-export SECRETD_STATESYNC_RPC_SERVERS="https://rpc.cosmos.directory:443/secretnetwork,https://rpc.cosmos.directory:443/secretnetwork"
+export SECRETD_STATESYNC_RPC_SERVERS="https://rpc.cosmos.directory:443/secretnetwork,http://secret.rpc.consensus.one:443"
 export SECRETD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export SECRETD_STATESYNC_TRUST_HASH=$TRUST_HASH
 export SECRETD_P2P_SEEDS="6fb7169f7630da9468bf7cc0bcbbed1eb9ed0d7b@scrt-seed-01.scrtlabs.com:26656,ab6394e953e0b570bb1deeb5a8b387aa0dc6188a@scrt-seed-02.scrtlabs.com:26656,9cdaa5856e0245ecd73bd464308fb990fbc53b57@scrt-seed-03.scrtlabs.com:26656"
