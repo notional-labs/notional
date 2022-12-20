03f8b0bb7a713004f08b2ab3813dbd70cfc2c11b
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-20 22:37:10 +0700

    Update buildmd.bash

--- incidents/buildmd.bash
+++ incidents/buildmd.bash
@@ -12,8 +12,6 @@ for f in *.md; do
 		echo "building '$f'"
     echo "    - running pandoc build script"
     pandoc "$f" -o "$f.pdf" --from markdown --template eisvogel.tex --listings
-    echo "    - generating preview"
-    pdftoppm -r 150 -png "document.pdf" > "preview.png"
-    echo ""
+
     	
 done
