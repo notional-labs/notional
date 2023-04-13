0ccdbf9f4950f84a43f34cdb3f7acb3d7bfc3de1
Long <77162856+mgl2150@users.noreply.github.com>
2023-04-13 23:26:27 +0700

    remove corruptive line

--- infrastructure/standup.bash
+++ infrastructure/standup.bash
@@ -10,12 +10,6 @@ echo 'GOPATH=/root/go' >> /root/.bashrc
 echo 'PATH=$PATH:/root/go/bin' >> /root/.bash_profile
 echo 'PATH=$PATH:/root/go/bin' >> /root/.bashrc
 
-# scroll up when using screen
-echo 'termcapinfo xterm* ti@:te@' > /root/.screenrc
-
-# Keys for arch packages
-pacman -Syyu archlinux-keyring
-
 
 # Utilities needed for smooth nodes
 pacman -Syyu aria2 atop autoconf automake base binutils bison bmon btrfs-progs btop clang cronie cryptsetup docker dstat fakeroot flex gcc git go gptfdisk groff grub haveged htop iftop iptraf-ng jq llvm lvm2 m4 make mdadm neovim net-tools nethogs openssh patch pkgconf python rsync rustup screen sudo texinfo unzip vi vim vnstat wget which xfsprogs hddtemp python-setuptools npm python-bottle python-docker python-matplotlib python-netifaces python-zeroconf python-pystache time nload nmon glances gtop bwm-ng bpytop duf go-ipfs fish pigz zerotier-one sysstat github-cli pm2 iotop nano git-lfs
